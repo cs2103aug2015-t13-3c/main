6dc3d2558a216f67c0501c5c7d1353815a21c7fa
Chin Kiat Boon <chinkiatboon91@gmail.com>
2015-09-21 15:57:26 +0800

    Initial commit for logic code

--- TextBuddy/TextBuddy/Logic.h
+++ TextBuddy/TextBuddy/Logic.h
@@ -1,14 +1,46 @@
-// @@author Chin Boon Kiat
+// @@author Chin Kiat Boon
 
 #ifndef LOGIC_H_
 #define LOGIC_H_
 
 #include "IO.h"
 
+#include <string>
+#include <iostream>
+#include <sstream>
+
+//assume max storage of 1000 tasks for now
+#define SIZE 1000
 class Logic{
 private:
 	bool isActive;
 	IO io;
+
+	//start and end dates to allow for "blocking" of slots for now
+	//if it is a normal "duedate", no endDate and endTime will be given
+	//for floating tasks, all the dates and times could be given impossible values, eg 999999 for date and 9999 for time 
+	/*class*/struct textInfo{							//change to class
+		std::string text;						//basic contents of task
+		int startDate;							//YYMMDD format (if year not parsed then can add 150000)
+		int startTime;							//24hr/HHMM format (or HMM if [12mn < time < 12nn])
+		int endDate;							//if no endDate, give it value of startDate
+		int endTime;							//if no endTime, give it value of startTime
+		//bool isPriority;						//add later when necessary					
+		//std::string label;					
+		//int ID;								//permanently tagged to a specific task
+		//bool isDone;
+/*
+	public:
+		std::string getText();
+		int getStartDate();
+		int getStartTime();
+		int getEndDate();
+		int getEndTime();*/
+	};
+
+	//found separate array
+	struct textInfo infoArray[SIZE];			//change to vector
+	int sizeOfArray;
 
 public:
 	Logic();
@@ -17,6 +49,23 @@ public:
 
 	bool getStatus();
 	bool canCallIO();
+
+	//the four methods below serve as 'intermediate' methods
+	int getSize(void);
+	void copyArray(struct textInfo stringArray[]);
+	void swapElement(textInfo& textA, textInfo& textB);
+	int addTime(int time, int addValue);
+
+	void addInfo(std::string textInput, int startDateInput, int startTimeInput, int endDateInput, int endTimeInput);
+	void deleteInfo(int index);
+	std::string returnInfo(int index);
+
+	//void sortDateAndTime(struct textInfo textArray[]);
+	void sortDateAndTime(void);
+	void changeInfo(std::string textInput, int startDateInput, int startTimeInput, int endDateInput, int endTimeInput, int index);
+	std::string freeSlotSearch(int date, int time);
+	//void blockSlots;
+	//std::string returnHelpText(void);
 };
 
 #endif
--- TextBuddy/TextBuddyTests/LogicTest.cpp
+++ TextBuddy/TextBuddyTests/LogicTest.cpp
@@ -18,5 +18,85 @@ namespace TextBuddyTests
 			Assert::AreEqual(true,status);
 		}
 
+		TEST_METHOD(AddAndReturnInfoTest) {
+			Logic testLogic;
+			
+			Assert::AreEqual(std::string("Nothing to return."), testLogic.returnInfo(2));
+
+			testLogic.addInfo(std::string("abc"), 150718, 1157, 150818, 1257);
+			testLogic.addInfo(std::string("def"), 150818, 1257, 150918, 1357);
+			testLogic.addInfo(std::string("Hello World!"), 150918, 1357, 151018, 1457);
+
+			Assert::AreEqual(std::string("Hello World!|150918|1357|151018|1457"), testLogic.returnInfo(2));
+		}
+
+		TEST_METHOD(DeleteInfoTest) {
+			Logic testLogic;
+
+			testLogic.addInfo(std::string("abc"), 150718, 1157, 150818, 1257);
+			testLogic.addInfo(std::string("def"), 150818, 1257, 150918, 1357);
+			testLogic.addInfo(std::string("Hello World!"), 150918, 1357, 151018, 1457);
+
+			testLogic.deleteInfo(1);
+			Assert::AreEqual(std::string("Hello World!|150918|1357|151018|1457"), testLogic.returnInfo(1));
+
+		}
+
+		TEST_METHOD(SortTest) {
+			Logic testLogic;
+
+			testLogic.addInfo(std::string("abc"), 150718, 1357, 150818, 1257);
+			testLogic.addInfo(std::string("def"), 150818, 957, 150918, 1357);
+			testLogic.addInfo(std::string("Hello World!"), 150808, 1057, 151018, 1457);
+
+			testLogic.sortDateAndTime();
+
+			Assert::AreEqual(std::string("Hello World!|150808|1057|151018|1457"), testLogic.returnInfo(1));
+		}
+
+		TEST_METHOD(freeSlotSearch) {
+			Logic testLogic;
+			std::string freeSlot;
+
+			freeSlot = testLogic.freeSlotSearch(150918,2143);
+
+			Assert::AreEqual(std::string("150918|2143"), freeSlot);
+
+			testLogic.addInfo(std::string("abc"), 150718, 1357, 150818, 1256);
+			testLogic.addInfo(std::string("def"), 150818, 1257, 150918, 1355);
+			testLogic.addInfo(std::string("Hello World!"), 150918, 1357, 151018, 1457);
+
+			freeSlot = testLogic.freeSlotSearch(150718,1355);
+
+			Assert::AreEqual(std::string("150718|1355"), freeSlot);
+
+			freeSlot = testLogic.freeSlotSearch(150718,1357);
+
+			Assert::AreEqual(std::string("150918|1356"), freeSlot);
+
+			freeSlot = testLogic.freeSlotSearch(140718,1357);
+
+			Assert::AreEqual(std::string("140718|1357"), freeSlot);
+		}
+
+		TEST_METHOD(AddTimeTest) {
+			Logic testLogic;
+			int number;
+
+			number = testLogic.addTime(2259, 1);
+			Assert::AreEqual(2300, number);
+		}
+
+		TEST_METHOD(ChangeInfoTest) {
+			Logic testLogic;
+
+			testLogic.addInfo(std::string("abc"), 150718, 1157, 150818, 1257);
+			testLogic.addInfo(std::string("def"), 150818, 1257, 150918, 1357);
+			testLogic.addInfo(std::string("Hello World!"), 150918, 1357, 151018, 1457);
+			testLogic.addInfo(std::string("Hello World!"), 150918, 1357, 151018, 1457);
+
+			testLogic.changeInfo(std::string("Bye World!"), 151126, 1300, 151126, 1500, 3);
+			Assert::AreEqual(std::string("Bye World!|151126|1300|151126|1500"), testLogic.returnInfo(3));
+		}
 	};
 }
