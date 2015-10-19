2c4df34f83bdedc2c306a7cd54b4055f82454195
Chin Kiat Boon <chinkiatboon91@gmail.com>
2015-10-20 07:27:26 +0800

    methods to search for label, free slots, and task names(within a specified period) added

--- TextBuddy/TextBuddy/PowerSearch.cpp
+++ TextBuddy/TextBuddy/PowerSearch.cpp
@@ -15,65 +15,92 @@ void PowerSearch::setTasksWithinPeriod(int startDate, int startTime, int endDate
 	
 	tasksWithinPeriod.clear();
 
-	while (iter->getStartDate() < startDate) {
-		++iter;
-	}
-
-	//if date is the same, further filter using time in the date
-	if (iter->getStartDate() == startDate) {
-		while (iter->getStartTime() < startTime) {
-			++iter;
-		}
-	}
-
-	while (iter->getStartDate() < endDate) {
-		tasksWithinPeriod.push_back(*iter);
-		++iter;
-	}
-
-	if (iter->getStartDate() == endDate) {
-		while (iter->getStartTime() < endTime) {
+	for (iter = taskStore.begin(); iter != taskStore.end(); ++iter) {
+		if ((iter->getStartDate() > startDate) && (iter->getStartDate() < endDate)) {
 			tasksWithinPeriod.push_back(*iter);
-			++iter;
+		}
+
+		//if date is the same, further filter using time in the date
+		if (iter->getStartDate() == startDate) {
+			if (iter->getStartTime() >= startTime) {
+				tasksWithinPeriod.push_back(*iter);
+			}
+		}
+
+		if (iter->getStartDate() == endDate) {
+			if (iter->getStartTime() <= endTime) {
+				tasksWithinPeriod.push_back(*iter);				
+			}
 		}
 	}
-
-	tasksWithinPeriod.pop_back();									//bring back one task to ensure tasks still lie within range
-
-	return;
 }
 
+void PowerSearch::addFreeDate(int startDate, int startTime, int endDate, int endTime) {
+	Task freeDateTask;
+
+	freeDateTask.setStartDate(startDate);
+	freeDateTask.setStartTime(startTime);
+	freeDateTask.setEndDate(endDate);
+	freeDateTask.setEndTime(endTime);
+
+	freeDates.push_back(freeDateTask);
+}
 
 
 //public methods
 //KB: will complete all 3 (and add more if possible) by tues morning
-void PowerSearch::searchInfo(std::string phr, int startDate, int endDate) {
+
+//searches for a phrase within a particular time period, stores the output in currentView
+void PowerSearch::searchInfo(std::string phr, int startDate, int startTime, int endDate, int endTime) {
 	std::vector<Task>::iterator iter;
+	std::string taskName;
 
-	for (iter = taskStore.begin(); iter != taskStore.end(); ++iter) {
-		/*
-	for (iter = taskStore.begin(); iter != taskStore.end(); ++iter) {
+	currentView.clear();
+	setTasksWithinPeriod(startDate, startTime, endDate, endTime);
+
+	for (iter = tasksWithinPeriod.begin(); iter != tasksWithinPeriod.end(); ++iter) {
 		taskName = iter->getName();
-		if(Utilities::isSubstring(searchPhrase,taskName)) {
-			id = iter->getID();
-			indexString << id << ",";
+		if (Utilities::isSubstring(phr,taskName)) {
+			currentView.push_back(*iter);
 		}
-	}
-	returnString = indexString.str();
-
-	if(!returnString.empty()) {
-		returnString.pop_back();
-	}
-
-	return returnString;*/
 	}
 }
 
-void PowerSearch::searchFreeSlot(int startDate, int endDate) {
-	
+void PowerSearch::searchFreeSlot(int startDate, int startTime, int endDate, int endTime) {
+	std::vector<Task>::iterator iter;
+	Task freeDateTask;
+	int freeDateStart = startDate;
+	int freeTimeStart = startTime;
+
+	setTasksWithinPeriod(startDate, startTime, endDate, endTime);
+
+	for (iter = tasksWithinPeriod.begin(); iter != tasksWithinPeriod.end(); ++iter) {
+		if ((iter->getStartDate() > freeDateStart) || ((iter->getStartDate() == freeDateStart) && (iter->getStartTime() >= freeTimeStart))) {
+			addFreeDate(freeDateStart, freeTimeStart, iter->getStartDate(), iter->getStartTime());
+		} 
+
+		freeDateStart = iter->getEndDate();			
+		freeTimeStart = iter->getEndTime();
+	}
 }
 
 void PowerSearch::searchLabel(std::string label) {
-	
+	std::set<std::string> searchSet;
+
+	std::vector<Task>::iterator taskIter;
+	std::set<std::string>::iterator setIter;
+
+	currentView.clear();
+
+	for (taskIter == taskStore.begin(); taskIter != taskStore.end(); ++taskIter) {
+		searchSet = taskIter->getLabels();
+
+		for (setIter == searchSet.begin(); setIter != searchSet.end(); ++setIter) {
+			if (*setIter == label) {
+				currentView.push_back(*taskIter);
+				break;
+			}
+		}
+	}
 }
 
