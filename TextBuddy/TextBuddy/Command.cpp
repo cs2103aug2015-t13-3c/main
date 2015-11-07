09792831a538b52bde7a6067df40d91d57d84a61
rz <andromeda143@hotmail.com>
2015-11-08 02:42:22 +0800

    Bug fix: view week

--- TextBuddy/TextBuddy/Command.cpp
+++ TextBuddy/TextBuddy/Command.cpp
@@ -1054,18 +1054,18 @@ void View::execute() {
 			((weekDate%10000)/100) == 8 || 
 			((weekDate%10000)/100) == 10 || 
 			((weekDate%10000)/100) == 12) {
-				if(weekDate > 31) {
+				if(weekDate%10 > 31) {
 					weekDate += 100 - 31;
 				}
 		} else if (((weekDate%10000)/100) == 4 || 
 			((weekDate%10000)/100) == 6 || 
 			((weekDate%10000)/100) == 9 || 
 			((weekDate%10000)/100) == 11) {
-				if(weekDate > 30) {
+				if(weekDate%10 > 30) {
 					weekDate += 100 - 30;
 				}
 		} else if (((weekDate%10000)/100) == 2) {
-			if(weekDate > 28) {
+			if(weekDate%10 > 28) {
 				weekDate += 100 - 28;
 			}
 		}
