9466748bf0a7a45a126a30583d782d8e4d273098
rz <andromeda143@hotmail.com>
2015-11-09 18:22:03 +0800

    Revert "Tried to fix 'on' and 'by' for modify but doesn't work (DONT USE)"
    This reverts commit 8f8cd46f438ee4a183886595621daaf7166ab98b.

--- TaskShark/TaskSharkInternal/Command.cpp
+++ TaskShark/TaskSharkInternal/Command.cpp
@@ -568,7 +568,7 @@ Modify::Modify(int taskID, std::vector<FieldType> fields,
 				   tempTask = task;
 				   invalidDateTimeString = restOfInput;
 				   initialiseIteratorsFromGuiID(modifyID);
-				   originalTask = *currViewIter;
+	originalTask = *currViewIter;
 }
 
 Modify::Modify(CommandType pick) : Command(pick) {}
@@ -667,16 +667,9 @@ void Modify::doModify() {
 		case PRIORITY_UNSET:
 			taskStoreIter->unsetPriority();
 			break;
-		case START_END_DATE:
 		case START_DATE:
 			taskStoreIter->setStartDate(tempTask.getStartDate());
-			if(*fieldIter != START_END_DATE) {
-				break;
-			}
-		case END_DATE:
-			taskStoreIter->setEndDate(tempTask.getEndDate());
 			break;
-		case START_END_TIME:
 		case START_TIME:
 			taskStoreIter->setStartTime(tempTask.getStartTime());
 			if ((fieldIter+1 != fieldsToModify.end()) && (fieldIter+2 != fieldsToModify.end())
@@ -684,9 +677,10 @@ void Modify::doModify() {
 					*(fieldIter+2) = END_TIME;
 			}
 			isStartTimeSet = true;
-			if(*fieldIter != START_END_TIME) {
-				break;
-			}
+			break;
+		case END_DATE:
+			taskStoreIter->setEndDate(tempTask.getEndDate());
+			break;
 		case END_TIME:
 			if (taskStoreIter->getEndDate() == DATE_NOT_SET) {
 				taskStoreIter->setEndDate(tempTask.getStartDate());
